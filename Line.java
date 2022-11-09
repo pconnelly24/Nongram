d3dc6c1f77b2a1befd9882e131f757a9e035bb99
Price Connelly <pconnelly24@students.hopkins.edu>
2022-11-08 21:43:38 -0500

    New plan, try everything

--- Line.java
+++ Line.java
@@ -17,63 +17,12 @@ public class Line {
         this.squares = inSquares;
     }
 
-    public int check(int inOrder){
-        int gap;
-        Group curGroup = groups[0];
-        for (int i = 0; i < squares.length; i++) {
-            switch(squares[i].getState()){
-                // The Square is Empty
-                case EMPTY:
-                    gap++;
-                break;
+    public boolean check(){
+        boolean changed = false;
 
-                // The Square is Filled
-                case FILLED:
+        
 
-                break;
-
-                // The Square is and X
-                case X:
-                    if(curGroup.getSize() == gap){
-                        for (int j = i - curGroup.getSize(); j < i; j++) {
-                            squares[j].setState(State.FILLED, inOrder);
-                            curGroup.setSquare(squares[j], j);
-                            inOrder ++;
-                        }
-                    }
-                    else if(curGroup.getSize() * 2 > gap){
-                        for (int j = i - (int)(curGroup.getSize() / 2f); j < i - (int)(curGroup.getSize() / 2f); j++) {
-                            squares[j].setState(State.FILLED, inOrder);
-                            curGroup.setSquare(squares[j], j);
-                            inOrder ++;
-                        }
-                    }
-                    gap = 0;
-                break;
-
-                // Uh oh
-                default:
-
-                break;
-            }
-        }
-        if(gap > 0){
-            if(curGroup.getSize() == gap){
-                for (int j = i - curGroup.getSize(); j < i; j++) {
-                    squares[j].setState(State.FILLED, inOrder);
-                    curGroup.setSquare(squares[j], j);
-                    inOrder ++;
-                }
-            }
-            else if(curGroup.getSize() * 2 > gap){
-                for (int j = i - (int)(curGroup.getSize() / 2f); j < i - (int)(curGroup.getSize() / 2f); j++) {
-                    squares[j].setState(State.FILLED, inOrder);
-                    curGroup.setSquare(squares[j], j);
-                    inOrder ++;
-                }
-            }
-        }
-        return inOrder;
+        return changed;
     }
 
     public Group[] getGroups(){
